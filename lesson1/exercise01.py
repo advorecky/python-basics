d37c6675cb09ee85425900434413a9171bb3ed9a
Aleksandr Dvoretskiy <al.dvorecky@gmail.com>
2021-03-08 11:41:18 +0300

    Create exercise01.py

---
+++ lesson1/exercise01.py
@@ -0,0 +1,14 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+hello_var = "Hello variable"
+digit_var = 100
+
+print(hello_var)
+print(digit_var)
+
+user_hello_var = input("Введите произвольную строку: ")
+user_digit_var = int(input("Введите произвольное число: "))
+
+print(f"Вы ввели строку: {user_hello_var}")
+print(f"Вы ввели число: {user_digit_var}")
